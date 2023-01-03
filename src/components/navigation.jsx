9f8a5ae6408ef2cfb2811bd3c48b50fd56ca3c46
omerfarukpolat <omerfarukpolat@etu.edu.tr>
2023-01-03 23:13:52 +0300

    logo saved as png

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            <img src='img/dentics_logo_s_1.jpg'  alt={''}/>
+            <img style={{marginTop: -10}}src='img/logo.png'  alt={''}/>
           </a>
         </div>
 
